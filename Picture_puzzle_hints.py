91a86822367b0ecfd52e3fd32523bb0258d0a2e2
Skyzeala <anderson.kirani@gmail.com>
2026-02-20 21:43:10 -0800

    added more pictures

--- Picture_puzzle_hints.py
+++ Picture_puzzle_hints.py
@@ -1,16 +1,15 @@
-# This script is designed to give synonyms to the pictures found on the estate
-# The goal is to give ideas and inspiration rather than the answer, but answers are present
-
 # !!! This script contains major spoilers for many phases of the game !!!
-# do not assume you have seen them all
+# The script itself does try to mitigate spoilers in outputs
 
-# The script will ask for a description of the picture, and give synonyms for the closest match
+# This script will give synonyms to the pictures found on the estate based on a description
+# The goal is to give ideas and inspiration rather than the answer which you'll still have to find
+# 
 
 # Some pictures are combined into one result, 
 #   for example saying "person" will lead to multiple painting answers, with some extras mixed in
 
-import re
-import random
+import re #regex for text manipulation
+import random #show results in a random order
 
 
 
@@ -18,15 +17,17 @@ print()
 print("Please enter one or more key words to describe the image.")
 print("For example, 'tree', or 'ace of spades'.")
 print()
+
 words = input("Description: ")
 
-words = re.sub('[^A-Za-z0-9\\s&]+', '', words)
+words = re.sub('[^A-Za-z0-9\\s&]+', '', words) #remove unwanted characters
 
 word_list = words.split()
 
 print("Looking for pictures matching: ", ", ".join(word_list))
 print()
 
+#set up variables for finding the best match
 high_score = 0
 score = 0
 matched_synonyms = []
@@ -34,6 +35,7 @@ many_matches = False
 
 
 
+#list of words for each picture
 picture_list = [
     ["ace of spades", "ace", "spade", "card", "play", "hand", "one"],
     ["queen of diamonds", "queen", "diamond", "card", "play", "hand", "face card", "face", 
@@ -43,16 +45,15 @@ picture_list = [
     ["crown", "royal", "king", "prince", "baron", "lord", "jewel", "treasure", "reign"],
     ["blueprint", "plan", "draft", "letter", "design", "diagram", "map", "layout", "outline"],
     ["tie", "necktie", "neck", "knot", "band", "scarf", "ruff", "strap"],
-    ["tiered cake holder", "tier", "cake", "bake"],
+    ["tiered cake holder", "tier", "cake", "bake", "dish", "layer", "layered", "three"],
     ["plot of land", "plot", "forest", "section"],
     ["person", "man", "woman", "pilot", "herbert", "chief", "chef", "cook", "baker", "bride", 
      "wife", "husband", "nurse", "agent", "beard", "farmer", "ranger", "lord", "lady", "miner"],
     ["hose", "garden hose", "pipe", "tube", "water", "sprinkler"],
     ["cliff", "sea", "ocean", "coast", "beach", "landscape", "hillside"],
     ["ten", "necklace", "tag", "price tag", "price", "cost", "luggage tag", "10"],
-    ["plane", "flier", "airplane", "skies", "sky", "clouds", "cloud", "fly"],
+    ["plane", "flier", "airplane", "skies", "sky", "clouds", "cloud", "fly", "flight"],
     ["cloud", "clouds", "rain", "hale", "weather", "sky"],
-    ["planet", "globe", "create", "art", "world", "paint", "brush", "hand", "earth", "realm"],
     ["plant", "flower", "herb", "greenery", "flora", "vegetable", "bloom", "sage", "basil", 
      "oregano", "thyme"],
     ["mountain", "hill", "top", "snowtop", "peak", "summit", "alp", "mt", "holly", "snow"],
@@ -84,8 +85,8 @@ picture_list = [
     ["dessert", "flan", "jello", "pudding", "spoon", "plate", "custard", "platter", "saucer"],
     ["boat", "rowboat", "row", "canoe", "raft", "paddle", "oar", "paddleboat", "cruise", "ferry", 
      "dinghy", "dory"],
-    ["and", "&", "also", "too", "with", "plus"],
-    ["cot", "bed", "hammock"],
+    ["and", "&", "also", "too", "with", "plus", "ampersand", "symbol"],
+    ["cot", "bed", "hammock", "sleep", "rest"],
     ["wheat", "grain", "bunch", "bundle", "spelt", "grass", "clump", "bind", "wreath"], 
     ["crate", "box", "wooden crate", "wood", "storage", "store"],
     ["road", "travel", "trek", "path", "highway", "roadway", "pavement", "street", "route", "way",
@@ -104,21 +105,28 @@ picture_list = [
     ["pin", "safety", "clip", "clamp", "tack", "clasp", "fasten"],
 
 
-
-
-    ["graph", "rate", "rise", "spike", "plot", "chart", "diagram", "table", "figure"],
+    ["pear", "fruit"],
+    ["spear", "pike", "sharp", "harpoon", "stab", "pierce", "lance", "poke", "impale", "jab",
+     "skewer", "shaft", "cut", "weapon", "blade", "edge"],
+    ["leash", "lead", "tether", "rope", "cord", "strap", "halter"],
+    ["planet", "globe", "create", "art", "world", "paint", "brush", "hand", "earth", "realm",
+     "sphere", "ball", "orb"],
+    ["graph", "rate", "rise", "spike", "plot", "chart", "diagram", "table", "figure", "peak"
+     "fall", "decline", "climb", "drop", "dip", "grow", "data", "bar", "line", "histogram"],
     ["book", "author", "novel", "story", "diary", "journal", "volume", "tome", "title", "chapter",
-     "mary", "bible", "holy"],
+     "mary", "bible", "holy", "spine"],
     ["christmas", "tree", "noel", "merry", "yule", "advent", "holiday", "gift", "present",
-     "presents"],
+     "presents", "pine"],
     ["shoelace", "end", "aglet", "lace"],
     ["calendar", "year", "month", "day", "date", "tear", "eight", "8"],
     ["thorn", "stem", "vine", "rose", "spike", "barb"],
-    ["ear", "lobe", "hear"],
+    ["ear", "lobe", "hear", "sound", "noise", "earlobe"],
     ["steam", "pot", "boil", "stock", "soup", "burn"],
+    ["sign", "post"],
     ["landscape", "stream"],
     ["trellis", "arch", "arbor"],
-    ["ship", "port", "harbor", "pier", "sail"],
+    ["ship", "port", "harbor", "pier", "sail", "sailboat", "boat", "launch"],
+    ["lunch", "box", "lunchbox", "tin", "meal"],
     ["music", "note", "song", "tune", "bar", "melody"],
     ["win", "medal", "one", "1", "1st place", "place", "first"],
     ["boar", "pig"],
@@ -126,26 +134,36 @@ picture_list = [
     ["farm", "ranch", "barn", "field"],
     ["fountain", "pen", "ink"],
     ["wine", "bottle", "cork"],
+    ["rat", "mouse", "rodent", "vermin", "varmint", "gnawer", "hamster"],
     ["tarot", "moon", "card", "the moon", "eclipse", "crescent"],
     ["bait", "worm", "fishing bait"],
     ["horse", "mustang", "stallion", "gallop", "trot", "canter", "run", "speed", "steed", 
      "racehorse", "bronco", "pony", "mount", "equine", "race", "hoof"],
     ["banana", "rot", "decay", "yellow", "brown"],
+    ["pi", "three", "3.14", "circle", "math", "symbol", "greek", "ratio"]
 ]
 
 
        
 for pic_word_list in picture_list:
-    score = len(set(word_list) & set(pic_word_list))
+    words_in_common = set(word_list) & set(pic_word_list)
+    score = len(words_in_common)
 
     if score > high_score:
         high_score = score
         matched_synonyms = pic_word_list
         many_matches = False
+
+        #if there is only one key word, only show the first result found
+        if len(word_list) < 2:
+            break
     elif score == high_score:
         many_matches = True
 
-if many_matches == True:
+if high_score == 0:
+    print("Could not find a matching image, try different descriptive words")
+    exit()
+elif many_matches == True:
     print("Too many matching pictures, try adding more descriptive words")
     exit()
 
